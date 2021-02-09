69d5b5f0239abcee5ed5b404bad212aa01b87061
blue sugai <taisei.s1257@icloud.com>
2021-02-09 18:21:46 +0900

    データベースの変更

--- lamp_dock/append_schema.sql
+++ lamp_dock/append_schema.sql
@@ -1,9 +1,8 @@
 CREATE TABLE `histories` (
     `history_id` int(11) NOT NULL AUTO_INCREMENT,
     `user_id` int(11) NOT NULL,
-    `item_id` int(11) NOT NULL,
     `created` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    'updated' datetime NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+    `updated` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
     primary key(history_id)
 );
 
@@ -13,5 +12,5 @@ CREATE TABLE `details` (
     `item_id` int(11) NOT NULL,
     `amount` int(11) NOT NULL,
     `created` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    'updated' datetime NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+    `updated` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
 ); 
